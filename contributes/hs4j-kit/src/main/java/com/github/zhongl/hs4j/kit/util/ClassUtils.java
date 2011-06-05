4c0b133808612c4727491160857521d4b86eb127
jushi <jushi@taobao.com>
2011-06-05 22:08:45 +0800

    Abstract ClassUtils and fix some javadoc.

---
+++ contributes/hs4j-kit/src/main/java/com/github/zhongl/hs4j/kit/util/ClassUtils.java
@@ -0,0 +1,22 @@
+package com.github.zhongl.hs4j.kit.util;
+
+import java.lang.reflect.*;
+
+/**
+ * {@link ClassUtils}
+ * 
+ * @author <a href=mailto:zhong.lunfu@gmail.com>zhongl</a>
+ * @created 2011-6-5
+ * 
+ */
+public class ClassUtils {
+  @SuppressWarnings("unchecked")
+  public static <T> Class<T> getGenericClassFrom(Type genericReturnType) throws ClassNotFoundException {
+    final String sigin = genericReturnType.toString();
+    final int begin = sigin.indexOf('<') + 1;
+    final int end = sigin.indexOf('>');
+    return (Class<T>) Class.forName(sigin.substring(begin, end));
+  }
+
+  private ClassUtils() {}
+}
