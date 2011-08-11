e9a56b6dca10d83ade65abdd4c0888c8a6bc33b1
jushi <jushi@taobao.com>
2011-08-11 14:10:39 +0800

    fix issue #15, and update README.

--- contributes/hs4j-kit/src/main/java/com/github/zhongl/hs4j/kit/util/StringUtils.java
+++ contributes/hs4j-kit/src/main/java/com/github/zhongl/hs4j/kit/util/StringUtils.java
@@ -10,18 +10,25 @@ import java.util.*;
  * 
  */
 public class StringUtils {
-  public static String[] toStringArray(Object... objects) {
-    String[] values = new String[objects.length];
-    for (int i = 0; i < values.length; i++) {
-      values[i] = objects[i].toString();
-    }
-    return values;
+  public static String join(String... strings) {
+    final StringBuilder sb = new StringBuilder();
+    for (final String string : strings)
+      sb.append(string);
+    return sb.toString();
   }
 
   public static String[] toStringArray(List<Object> objects) {
     String[] values = new String[objects.size()];
     for (int i = 0; i < values.length; i++) {
-      values[i] = objects.get(i).toString();
+      values[i] = string(objects.get(i));
+    }
+    return values;
+  }
+
+  public static String[] toStringArray(Object... objects) {
+    String[] values = new String[objects.length];
+    for (int i = 0; i < values.length; i++) {
+      values[i] = string(objects[i]);
     }
     return values;
   }
@@ -29,15 +36,12 @@ public class StringUtils {
   public static String[] toStringArray(Object[] objects, int from, int to) {
     String[] values = new String[to - from];
     for (int i = 0; i < values.length; i++) {
-      values[i] = objects[i + from].toString();
+      values[i] = string(objects[i + from]);
     }
     return values;
   }
 
-  public static String join(String... strings) {
-    final StringBuilder sb = new StringBuilder();
-    for (final String string : strings)
-      sb.append(string);
-    return sb.toString();
+  private static String string(Object object) {
+    return object == null ? null : object.toString();
   }
 }
